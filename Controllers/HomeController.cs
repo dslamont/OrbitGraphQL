8611677dffdc4fd327eeb876b79673d1091efd58
dslamont <don.lamont@live.co.uk>
2024-11-26 12:37:45 +0000

    Upgraded to .Net 9. Tidied up

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrbitGraphQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "GraphQL Test";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,32 +21,39 @@ namespace OrbitGraphQL
                 .AddSystemTextJson());   // serializer
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.MapGraphQL();
             }
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
             app.MapControllers();
 
+            app.MapGet("/", () => "GraphQL Example");
+
+
             app.UseGraphQL<ISchema>("/graphql");            // url to host GraphQL endpoint
-            app.UseGraphQLPlayground(
-                "/",                               // url to host Playground at
-                new GraphQL.Server.Ui.Playground.PlaygroundOptions
-                {
-                    GraphQLEndPoint = "/graphql",         // url of GraphQL endpoint
-                    SubscriptionsEndPoint = "/graphql",   // url of GraphQL endpoint
-                });
+            //app.UseGraphQLPlayground(
+            //    "/",                               // url to host Playground at
+            //    new GraphQL.Server.Ui.Playground.PlaygroundOptions
+            //    {
+            //        GraphQLEndPoint = "/graphql",         // url of GraphQL endpoint
+            //        SubscriptionsEndPoint = "/graphql",   // url of GraphQL endpoint
+            //    });
 
             app.UseGraphQLGraphiQL();
 
